49defd1ccc65fe4f9c49cede1408ca8a25d404e9
littleblack57 <littleblack57@hotmail.com>
2016-03-10 10:44:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/black/mymymymy/my.java
@@ -0,0 +1,13 @@
+package com.example.student.black.mymymymy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class my extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
